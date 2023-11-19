b51240d6084b456f5b1cc68a4e240a35f14e64c9
Abdulkadir Gursoy <131687998+Kadir-Gursoy@users.noreply.github.com>
2023-11-19 22:37:49 +0300

    Update authApiCall.js

--- src/service/authApiCall.js
+++ src/service/authApiCall.js
@@ -2,19 +2,19 @@ import axios from "axios"
 import { useNavigate } from "react-router-dom"
 import { useDispatch } from "react-redux"
 
-//1 bir hook sadece bir react componeneti ve bir custom hook içerisidnde çağrılabilir. Bir JS func. içerisinde HOOK çağrılamaz
+//Bir hook sadece bir react componeneti ve bir custom hook içerisinde çağrılabilir. Bir JS func. içerisinde HOOK çağrılamaz
 export const login = async(userData) => {
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
-const BASE_URL ="http://10001.fullstack.clarusway.com"
+const BASE_URL ="http://14142.fullstack.clarusway.com"
 
 dispatch(fetchStart())
 
     try {
         await axios.post(`{BASE_URL}//account/auth/login/`, userData)
         dispatch(loginSucces(data))
-        toastSuccessNotify("login başaralı")
+        toastSuccessNotify("Giriş Başaralı")
         navigate("/")
     } catch (error) {
        dispatch(fetchFail())
