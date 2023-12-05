7f93f86817989a17117c8dcbaf35315fd95b0f43
Abdulkadir Gursoy <131687998+Kadir-Gursoy@users.noreply.github.com>
2023-12-05 23:29:44 +0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,7 +16,7 @@ function Dashboard() {
       <CssBaseline />
       <AppBar position="fixed">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
             STOCK APP
           </Typography>
           {currentUser && <Button color="inherit">Logout</Button>}
